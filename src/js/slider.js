dc8320fa47c5f82c56c1124f90910da2a502f4a5
VitaliiPshenychniy <4kvaap@gmail.com>
2023-02-01 10:36:49 +0200

    After mentor

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,12 +1,13 @@
-var swiper = new Swiper('.swiper_about-restaurant', {
+// функціонал слайдеру секції about-restaurant
+var swiper = new Swiper('.js-swiper_about-restaurant', {
   slidesPerView: 2,
   spaceBetween: 25,
   slidesPerGroup: 1,
   loop: true,
   loopFillGroupWithBlank: true,
   navigation: {
-    nextEl: '.swiper-button-next_new',
-    prevEl: '.swiper-button-prev_new',
+    nextEl: '.js-swiper-button-next',
+    prevEl: '.js-swiper-button-prev',
   },
   breakpoints: {
     1024: {
@@ -15,15 +16,16 @@ var swiper = new Swiper('.swiper_about-restaurant', {
   },
 });
 
-var swiper = new Swiper('.swiper_about-hotel', {
+// функціонал слайдеру секції about-hotel
+var swiper = new Swiper('.js-swiper_about-hotel', {
   slidesPerView: 2,
   spaceBetween: 25,
   slidesPerGroup: 1,
   loop: true,
   loopFillGroupWithBlank: true,
   navigation: {
-    nextEl: '.swiper2-button-next_new',
-    prevEl: '.swiper2-button-prev_new',
+    nextEl: '.js-swiper2-button-next',
+    prevEl: '.js-swiper2-button-prev',
   },
   breakpoints: {
     1024: {
@@ -32,18 +34,37 @@ var swiper = new Swiper('.swiper_about-hotel', {
   },
 });
 
-var swiper = new Swiper('.swiper_room-hero', {
-  direction: 'horizontal',
-  slidesPerView: 1,
-  spaceBetween: 0,
+// функціонал слайдеру секції about-kitchent
+var swiper = new Swiper('.js-swiper_about-kitchen', {
+  slidesPerView: 2,
+  spaceBetween: 25,
   slidesPerGroup: 1,
   loop: true,
   loopFillGroupWithBlank: true,
-  autoplay: {
-    delay: 3000,
-  },
   navigation: {
-    nextEl: '.swiper-button-next-room',
-    prevEl: '.swiper-button-prev-room',
+    nextEl: '.js-swiper3-button-next',
+    prevEl: '.js-swiper3-button-prev',
+  },
+  breakpoints: {
+    1024: {
+      spaceBetween: 40,
+    },
   },
 });
+
+
+// var swiper = new Swiper('.swiper_room-hero', {
+//   direction: 'horizontal',
+//   slidesPerView: 1,
+//   spaceBetween: 0,
+//   slidesPerGroup: 1,
+//   loop: true,
+//   loopFillGroupWithBlank: true,
+//   autoplay: {
+//     delay: 3000,
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next-room',
+//     prevEl: '.swiper-button-prev-room',
+//   },
+// });
